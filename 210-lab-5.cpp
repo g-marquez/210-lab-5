b1ba361fd937ac3de381cfd04d60c688294ab184
Gabe Marquez <gabrielmarquez98@gmail.com>
2026-02-12 17:25:30 -0800

    complete

--- 210-lab-5.cpp
+++ 210-lab-5.cpp
@@ -11,19 +11,25 @@ int swap(int *, int *);
 
 int main() {
     int *xptr, *yptr = nullptr; //be good stewards of our pointers
-    int x = 5, y = 10;
+    int x = 5, y = 10; //kept original values from starter code
+
+    //assign addresses of corresponding values to pointers
     xptr = &x;
     yptr = &y;
 
+    //switched x & y variables to dereferenced pointers
     cout << "x = " << *xptr << "  | y = " << *yptr << endl;
 
     cout << "Swapping...\n";
-    int sum = swap(xptr, yptr);
+    int sum = swap(xptr, yptr); //corrected function arguments
+
+    //switched x & y variables to dereferenced pointers, now swapped
     cout << "x = " << *xptr << " | y = " << *yptr << endl;
     cout << "sum = " << sum << endl;
     return 0;
 }
 
+//rewritten function such that pointers are used
 int swap(int *a, int *b) {
     int temp = *a;
     *a = *b;
